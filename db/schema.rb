3690804158b3beaf9b5efa9ef513ed037c3f074e
jiro.kirameki <soranonaka39@yahoo.co.jp>
2017-08-11 11:34:47 +0000

    bootstrap design

--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,12 @@ ActiveRecord::Schema.define(version: 20170731053215) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "title"
     t.string   "status"
+    t.index ["user_id"], name: "index_tasks_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
